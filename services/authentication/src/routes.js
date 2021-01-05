cc4fc9f414c9b7bcd0317b971b1930717777ec1d
GabsLoops <gabslopes34@gmail.com>
2021-01-05 10:47:31 -0300

    Basic structure for api's

---
+++ services/authentication/src/routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+router.get('/', (req, res) => {
+   const doc = {
+      api: 'Teste Técnico - GPTW - Serviço Authentication',
+      repository: 'https://github.com/gabriellopesdev/test_gptw',
+    }
+    res.status(200).json(doc)
+  })
+
+module.exports = router
